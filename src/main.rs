d8d5d22c8fc492d680d3616b2a3e0ee3fe183a1f
allaboutevemirolive <xkmxlfirdxus@gmail.com>
2024-02-11 08:37:32 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod fetcher;
+
 #[allow(unused_imports)]
 use crate::fetcher::*;
 
@@ -7,14 +8,21 @@ pub mod error;
 use crate::error::AppError;
 #[allow(unused_imports)]
 use axum::{
-    extract::FromRequest,
-    extract::Query,
-    extract::{rejection::ExtensionRejection, Extension},
+    extract::{
+        rejection::{ExtensionRejection, JsonRejection},
+        Extension, FromRequest, Query,
+    },
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
     Json, Router,
 };
+
+#[allow(unused_imports)]
+use axum_macros::FromRequest;
+
+#[allow(unused_imports)]
+use serde_json::json;
 
 #[allow(unused_imports)]
 use axum_macros::{debug_handler, FromRequestParts};
@@ -24,14 +32,30 @@ use serde::Deserialize;
 #[allow(unused_imports)]
 use std::{collections::HashMap, net::SocketAddr};
 
-async fn index() -> &'static str {
-    "Index"
+use askama::Template;
+
+// async fn index() -> &'static str {
+//     "Index"
+// }
+
+#[derive(Template)]
+#[template(path = "index.html")]
+#[derive(Deserialize)]
+struct IndexTemplate;
+
+#[debug_handler]
+async fn index() -> IndexTemplate {
+    IndexTemplate
 }
 
-#[derive(Deserialize, FromRequestParts)]
+impl IntoResponse for IndexTemplate {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, "something went wrong").into_response()
+    }
+}
 
+#[derive(Deserialize)]
 pub struct WeatherQuery {
-    #[from_request(via(Extension))]
     pub city: String,
 }
 
@@ -41,13 +65,58 @@ impl IntoResponse for WeatherQuery {
     }
 }
 
+// impl<B> FromRequest<B> for Query<WeatherQuery>
+// where
+//     B: Send,
+// {
+//     type Rejection = YourRejectionType;
+
+//     fn from_request(req: &mut Request, _: &mut Context) -> Poll<Result<Self, Self::Rejection>> {
+//         // Extract the `WeatherQuery` from the request here
+//         // For example, you can extract it from the query parameters
+//         if let Some(query) = req.uri().query() {
+//             if let Ok(weather_query) = serde_urlencoded::from_str(query) {
+//                 return Poll::Ready(Ok(Query(weather_query)));
+//             }
+//         }
+//         // If extraction fails, return a rejection
+//         Poll::Ready(Err(YourRejectionType))
+//     }
+// }
+
+// impl From<JsonRejection> for WeatherQuery {
+//     fn from(rejection: JsonRejection) -> Self {
+//         let response = (
+//             StatusCode::INTERNAL_SERVER_ERROR,
+//             axum::Json(json!({"error": rejection.to_string()})),
+//         )
+//             .into_response();
+
+//         WeatherQuery { city: response }
+//     }
+// }
+
+// #[debug_handler]
+// async fn weather(Query(params): Query<WeatherQuery>) -> Result<WeatherDisplay, AppError> {
+//     let lat_long = fetch_lat_long(&params.city).await?;
+
+//     let weather = fetch_weather(lat_long).await?;
+//     Ok(WeatherDisplay::new(params.city, weather))
+// }
+
 #[debug_handler]
 async fn weather(Query(params): Query<WeatherQuery>) -> Result<WeatherDisplay, AppError> {
     let lat_long = fetch_lat_long(&params.city).await?;
-
     let weather = fetch_weather(lat_long).await?;
     Ok(WeatherDisplay::new(params.city, weather))
 }
+
+// async fn weather(Query(params): Query<WeatherQuery>) -> Result<String, AppError> {
+//     let lat_long = fetch_lat_long(&params.city).await?;
+//     let weather = fetch_weather(lat_long).await?;
+//     let display = WeatherDisplay::new(params.city, weather);
+//     Ok(format!("{:?}", display))
+// }
 
 async fn stats() -> &'static str {
     "Stats"
